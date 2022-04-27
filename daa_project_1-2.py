cf7525db0ee3c9d53732dfb11f5340cc93099a8d
gbaser54 <68541784+gbaser54@users.noreply.github.com>
2022-04-26 21:01:25 -0400

    Update daa_project_1-2.py

--- daa_project_1-2.py
+++ daa_project_1-2.py
@@ -1,3 +1,4 @@
+#libraries install 
 %pip install scikit-image
 %pip install skimage
 %pip install ssl
@@ -8,55 +9,55 @@ import math
 from skimage import io, util
 import heapq
 
+             #Finding out the random patch
+def randomPatch_1(texture_portion, patchLengthsegment):
+    h1, w1, _ = texture_portion.shape
+    i = np.random.randint(h1 - patchLengthsegment)
+    j = np.random.randint(w1 - patchLengthsegment)
 
-def randomPatch_1(texture, patchLength):
-    h, w, _ = texture.shape
-    i = np.random.randint(h - patchLength)
-    j = np.random.randint(w - patchLength)
+    return texture_portion[i:i+patchLengthsegment, j:j+patchLengthsegment]
 
-    return texture[i:i+patchLength, j:j+patchLength]
-
-
-def L2OverlapDiff_1(patch, patchLength, overlap, res, y, x):
+          #Finding out the overlapping difference
+def L2OverlapDiff_1(patch, patchLengthsegment, overlap, res, y1, x1):
     error = 0
 
-    if x > 0:
-        left = patch[:, :overlap] - res[y:y+patchLength, x:x+overlap]
-        error = error + np.sum(left**2)
-    if y > 0:
-        up   = patch[:overlap, :] - res[y:y+overlap, x:x+patchLength]
+    if x1 > 0:
+        leftpart = patch[:, :overlap] - res[y1:y1+patchLengthsegment, x1:x1+overlap]
+        error = error + np.sum(leftpart**2)
+    if y1 > 0:
+        up   = patch[:overlap, :] - res[y1:y1+overlap, x1:x1+patchLengthsegment]
         error = error + np.sum(up**2)
 
-    if x > 0 and y > 0:
-        corner = patch[:overlap, :overlap] - res[y:y+overlap, x:x+overlap]
+    if x1 > 0 and y1 > 0:
+        corner = patch[:overlap, :overlap] - res[y1:y1+overlap, x1:x1+overlap]
         error = error - np.sum(corner**2)
 
     return error
  
-
-def randomBestPatch_1(texture, patchLength, overlap, res, y, x):
-    h, w, _ = texture.shape
-    errors = np.zeros((h - patchLength, w - patchLength))
+                  #Finding out the best random patch in all
+def randomBestPatch_1(texture_portion, patchLengthsegment, overlap, res, y1, x1):
+    h1, w1, _ = texture_portion.shape
+    errors = np.zeros((h1 - patchLengthsegment, w1 - patchLengthsegment))
     i=0
-    while i<(h - patchLength):
+    while i<(h1 - patchLengthsegment):
         j=0
-        while j<(w - patchLength):                                    
-            patch = texture[i:i+patchLength, j:j+patchLength]
-            e = L2OverlapDiff_1(patch, patchLength, overlap, res, y, x)
+        while j<(w1 - patchLengthsegment):                                    
+            patch = texture_portion[i:i+patchLengthsegment, j:j+patchLengthsegment]
+            e = L2OverlapDiff_1(patch, patchLengthsegment, overlap, res, y1, x1)
             errors[i, j] = e
             j=j+1
         i=i+1
     i, j = np.unravel_index(np.argmin(errors), errors.shape)
-    return texture[i:i+patchLength, j:j+patchLength]
+    return texture_portion[i:i+patchLengthsegment, j:j+patchLengthsegment]
 
 
-
+      #Applying the min-cut
 def minCutPath_1(errors):
-    # dijkstra's algorithm vertical
+    # Dijkstra's algorithm vertical
     pq = [(error, [i]) for i, error in enumerate(errors[0])]
     heapq.heapify(pq)
 
-    h, w = errors.shape
+    h1, w1 = errors.shape
     seen = set()
 
     while pq:
@@ -64,13 +65,13 @@ def minCutPath_1(errors):
         curDepth = len(path)
         curIndex = path[-1]
 
-        if curDepth == h:
+        if curDepth == h1:
             return path
     
         for delta in -1, 0, 1:
             nextIndex = curIndex + delta
 
-            if 0 <= nextIndex and nextIndex < w:
+            if 0 <= nextIndex and nextIndex < w1:
                 if (curDepth, nextIndex) not in seen:
                     cumError = error + errors[curDepth, nextIndex]
                     heapq.heappush(pq, (cumError, path + [nextIndex]))
@@ -78,7 +79,7 @@ def minCutPath_1(errors):
         
 
 def minCutPath2(errors):
-    # dynamic programming, used
+    # Dynamic programming, used
     errors = np.pad(errors, [(0, 0), (1, 1)], 
                     mode='constant', 
                     constant_values=np.inf)
@@ -92,7 +93,7 @@ def minCutPath2(errors):
         L = np.roll(M, 1)
         R = np.roll(M, -1)
 
-        # optimize with np.choose?
+        # Optimize with np.choose?
         cumError = np.min((L, M, R), axis=0) + errors[i]
         paths[i] = np.argmin((L, M, R), axis=0)
         i=i+1
@@ -103,57 +104,57 @@ def minCutPath2(errors):
     while i!=(reversed(len(errors))):
         minCutPath_1.append(minCutPath_1[-1] + paths[i][minCutPath_1[-1]])
         i=i+1
-    return map(lambda x: x - 1, reversed(minCutPath_1))
+    return map(lambda x1: x1 - 1, reversed(minCutPath_1))
 
-
-def minCutPatch_1(patch, patchLength, overlap, res, y, x):
+          #Getting the minimum cut patch 
+def minCutPatch_1(patch, patchLengthsegment, overlap, res, y1, x1):
     patch = patch.copy()
     dy, dx, _ = patch.shape
     minCut = np.zeros_like(patch, dtype=bool)
 
-    if x > 0:
-        left = patch[:, :overlap] - res[y:y+dy, x:x+overlap]
-        leftL2 = np.sum(left**2, axis=2)
+    if x1 > 0:
+        leftpart = patch[:, :overlap] - res[y1:y1+dy, x1:x1+overlap]
+        leftL2 = np.sum(leftpart**2, axis=2)
         for i, j in enumerate(minCutPath_1(leftL2)):
             minCut[i, :j] = True
 
-    if y > 0:
-        up = patch[:overlap, :] - res[y:y+overlap, x:x+dx]
+    if y1 > 0:
+        up = patch[:overlap, :] - res[y1:y1+overlap, x1:x1+dx]
         upL2 = np.sum(up**2, axis=2)
         for j, i in enumerate(minCutPath_1(upL2.T)):
             minCut[:i, j] = True
 
-    np.copyto(patch, res[y:y+dy, x:x+dx], where=minCut)
+    np.copyto(patch, res[y1:y1+dy, x1:x1+dx], where=minCut)
 
     return patch
 
 
-def quilt(texture, patchLength, numPatches, mode="cut", sequence=False):
-    texture = util.img_as_float(texture)
+def quilt(texture_portion, patchLengthsegment, numPatches, mode="cut", sequence=False):
+    texture_portion = util.img_as_float(texture_portion)
 
-    overlap = patchLength // 6
+    overlap = patchLengthsegment // 6
     numPatchesHigh_1, numPatchesWide_1 = numPatches
 
-    h = (numPatchesHigh_1 * patchLength) - (numPatchesHigh_1 - 1) * overlap
-    w = (numPatchesWide_1 * patchLength) - (numPatchesWide_1 - 1) * overlap
+    h1 = (numPatchesHigh_1 * patchLengthsegment) - (numPatchesHigh_1 - 1) * overlap
+    w1 = (numPatchesWide_1 * patchLengthsegment) - (numPatchesWide_1 - 1) * overlap
 
-    res = np.zeros((h, w, texture.shape[2]))
+    res = np.zeros((h1, w1, texture_portion.shape[2]))
     i=0
     while i!=(numPatchesHigh_1):
         j=0
         while j!=(numPatchesWide_1):
-            y = i * (patchLength - overlap)
-            x = j * (patchLength - overlap)
+            y1 = i * (patchLengthsegment - overlap)
+            x1 = j * (patchLengthsegment - overlap)
 
             if i == 0 and j == 0 or mode == "random":
-                patch = randomPatch_1(texture, patchLength)
+                patch = randomPatch_1(texture_portion, patchLengthsegment)
             elif mode == "best":
-                patch = randomBestPatch_1(texture, patchLength, overlap, res, y, x)
+                patch = randomBestPatch_1(texture_portion, patchLengthsegment, overlap, res, y1, x1)
             elif mode == "cut":
-                patch = randomBestPatch_1(texture, patchLength, overlap, res, y, x)
-                patch = minCutPatch_1(patch, patchLength, overlap, res, y, x)
+                patch = randomBestPatch_1(texture_portion, patchLengthsegment, overlap, res, y1, x1)
+                patch = minCutPatch_1(patch, patchLengthsegment, overlap, res, y1, x1)
             
-            res[y:y+patchLength, x:x+patchLength] = patch
+            res[y1:y1+patchLengthsegment, x1:x1+patchLengthsegment] = patch
 
             if sequence:
                 io.imshow(res)
@@ -163,15 +164,15 @@ def quilt(texture, patchLength, numPatches, mode="cut", sequence=False):
     return res
 
 
-def quiltSize(texture, patchLength, shape, mode="cut"):
-    overlap = patchLength // 6
-    h, w = shape
+def quiltSize(texture_portion, patchLengthsegment, shape, mode="cut"):
+    overlap = patchLengthsegment // 6
+    h1, w1 = shape
 
-    numPatchesHigh_1 = math.ceil((h - patchLength) / (patchLength - overlap)) + 1 or 1
-    numPatchesWide_1 = math.ceil((w - patchLength) / (patchLength - overlap)) + 1 or 1
-    res = quilt(texture, patchLength, (numPatchesHigh_1, numPatchesWide_1), mode)
+    numPatchesHigh_1 = math.ceil((h1 - patchLengthsegment) / (patchLengthsegment - overlap)) + 1 or 1
+    numPatchesWide_1 = math.ceil((w1 - patchLengthsegment) / (patchLengthsegment - overlap)) + 1 or 1
+    res = quilt(texture_portion, patchLengthsegment, (numPatchesHigh_1, numPatchesWide_1), mode)
 
-    return res[:h, :w]
+    return res[:h1, :w1]
 
 
 # In[4]:
